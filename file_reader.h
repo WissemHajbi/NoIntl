55d634c98a460e9b67990140cc8df953ca1ed9d6
Wissem Hajbi <wissemhajbi2002@gmail.com>
2026-02-14 22:48:51 +0100

    :art: reformat code with new clangd lsp from neovim

--- file_reader.h
+++ file_reader.h
@@ -5,20 +5,20 @@
 
 /* File buffer structure for reading file contents */
 typedef struct {
-    char *content;      /* The actual file content */
-    size_t size;        /* Number of bytes read */
-    size_t capacity;    /* Allocated buffer space */
+  char *content;   /* The actual file content */
+  size_t size;     /* Number of bytes read */
+  size_t capacity; /* Allocated buffer space */
 } FileBuffer;
 
 /* File reading configuration */
 typedef struct {
-    size_t initial_capacity;   /* Starting buffer size */
-    size_t max_file_size;      /* Maximum file size to read (safety limit) */
-    int follow_symlinks;       /* Follow symbolic links? */
+  size_t initial_capacity; /* Starting buffer size */
+  size_t max_file_size;    /* Maximum file size to read (safety limit) */
+  int follow_symlinks;     /* Follow symbolic links? */
 } FileReadConfig;
 
 /* Function declarations */
-FileBuffer* fb_create(size_t initial_capacity);
+FileBuffer *fb_create(size_t initial_capacity);
 int fb_read_file(const char *filepath, FileBuffer *buffer);
 void fb_free(FileBuffer *buffer);
 void fb_print_preview(const FileBuffer *buffer, size_t max_chars);
